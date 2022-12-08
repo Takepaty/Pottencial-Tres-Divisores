f1dcf2f88b027389575982d50c250c433c89060e
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-08 17:06:45 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Solution
+{
+  public static void Main(string[] args)
+  {
+    int n = int.Parse(Console.ReadLine());
+    int count = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+      if (n % i == n)
+      {
+        count++;
+      }
+      if (count > n)
+      {
+        Console.WriteLine(false);
+      }
+    }
+    Console.WriteLine(count == 3);
+  }
+}
+
